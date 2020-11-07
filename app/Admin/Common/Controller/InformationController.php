b8dfbb99822a0bde92ec0d032e7201ba92656ac2
webphplove <1346233126@qq.com>
2020-11-07 16:43:42 +0800

    修改显示时间

--- app/Admin/Common/Controller/InformationController.php
+++ app/Admin/Common/Controller/InformationController.php
@@ -4,16 +4,10 @@
 namespace App\Admin\Common\Controller;
 
 use App\Admin\Common\Util\Utils;
-use App\Admin\Util\ResultData;
-use Swoft\Co;
 use Swoft\Db\DB;
-use Swoft\Http\Message\Request;
-use Swoft\Http\Message\Response;
 use Swoft\Http\Server\Annotation\Mapping\Controller;
 use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
 use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
-use Swoft\Log\Helper\CLog;
-use Swoft\Log\Helper\Log;
 use Swoft\Stdlib\Helper\Sys;
 
 /**
@@ -23,16 +17,16 @@ class InformationController
 {
     public static $stats =
         [
-            'start_time'            => '服务器启动时间',
-            'connection_num'        => '当前连接的数量',
-            'accept_count'          => '接受连接数',
-            'close_count'           => '当前关闭的连接数量',
-            'tasking_num'           => '当前正在排队的任务数',
-            'request_count'         => 'Server收到的请求次数',
-            'worker_request_count'  => '当前Worker进程收到的请求次数',
+            'start_time' => '服务器启动时间',
+            'connection_num' => '当前连接的数量',
+            'accept_count' => '接受连接数',
+            'close_count' => '当前关闭的连接数量',
+            'tasking_num' => '当前正在排队的任务数',
+            'request_count' => 'Server收到的请求次数',
+            'worker_request_count' => '当前Worker进程收到的请求次数',
             'worker_dispatch_count' => 'master进程向当前Worker进程投递任务的计数',
-            'coroutine_num'         => '当前协程数量',
-            'worker_num'            => '当前进程启动数量',
+            'coroutine_num' => '当前协程数量',
+            'worker_num' => '当前进程启动数量',
         ];
 
     /**
@@ -42,17 +36,17 @@ class InformationController
     {
         $mysqlVersion = DB::select('select version()');
         return [
-            'mysqlVersion'       => substr($mysqlVersion[0]['version()'], 0, 6),
-            'upSize'             => Utils::getSize(env('PACKAGE_MAX_LENGTH')),
+            'mysqlVersion' => substr($mysqlVersion[0]['version()'], 0, 6),
+            'upSize' => Utils::getSize(env('PACKAGE_MAX_LENGTH')),
             'max_execution_time' => ($max = ini_get('max_execution_time') == 0) ? "不限制" : $max,
-            'free_space'         => Utils::getSize(disk_free_space('/')),
-            'env'                => [
-                'os'            => \PHP_OS,
-                'phpVersion'    => \PHP_VERSION,
+            'free_space' => Utils::getSize(disk_free_space('/')),
+            'env' => [
+                'os' => \PHP_OS,
+                'phpVersion' => \PHP_VERSION,
                 'swooleVersion' => \SWOOLE_VERSION,
-                'swoftVersion'  => \Swoft::VERSION,
+                'swoftVersion' => \Swoft::VERSION,
             ],
-            'swoole'             => \server()->getSetting(),
+            'swoole' => \server()->getSetting(),
         ];
     }
 
@@ -70,9 +64,12 @@ class InformationController
         $data = [];
         foreach ($stat as $k => $v) {
             if (isset(self::$stats[$k])) {
+                if ($k == 'start_time') {
+                    $v = date("Y-m-d H:i", $v);
+                }
                 $data[] = [
-                    'value'       => $v,
-                    'key'         => $k,
+                    'value' => $v,
+                    'key' => $k,
                     'description' => self::$stats[$k],
                 ];
 
@@ -105,12 +102,12 @@ class InformationController
      */
     private function formatSwooleInfo(string $str): array
     {
-        $data  = [];
-        $lines = \explode("\n", \trim($str));
+        $data = [];
+        $lines = explode("\n", \trim($str));
         foreach ($lines as $line) {
-            [$name, $value] = \explode(' => ', $line);
+            [$name, $value] = explode(' => ', $line);
             $data[] = [
-                'name'  => $name,
+                'name' => $name,
                 'value' => $value,
             ];
         }
@@ -131,13 +128,13 @@ class InformationController
             return ['code' => 404, 'msg' => $error];
         }
         // format
-        $str = \str_replace("\r\n", "\n", \trim($return));
+        $str = str_replace("\r\n", "\n", \trim($return));
         [, $enableStr, $directiveStr] = \explode("\n\n", $str);
         $directive = $this->formatSwooleInfo($directiveStr);
-        \array_shift($directive);
+        array_shift($directive);
         return [
-            'raw'       => $return,
-            'enable'    => $this->formatSwooleInfo($enableStr),
+            'raw' => $return,
+            'enable' => $this->formatSwooleInfo($enableStr),
             'directive' => $directive,
         ];
     }
@@ -150,7 +147,7 @@ class InformationController
      */
     public function phpExt(): array
     {
-        return \get_loaded_extensions();
+        return get_loaded_extensions();
     }
 
 }
